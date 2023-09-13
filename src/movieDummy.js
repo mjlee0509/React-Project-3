c07d5a017ffffad3a34014227d44ba6c1b62690f
mjlee0509 <mjlee0509@gmail.com>
2023-09-13 16:35:34 +0900

    영화 앱 만들기 II #4. 지난번에 썼던 더미데이터 가져오기 - react-project-1 참고

---
+++ src/movieDummy.js
@@ -0,0 +1,416 @@
+export const dummy =
+{
+    "dates": {
+      "maximum": "2023-08-22",
+      "minimum": "2023-07-05"
+    },
+    "page": 1,
+    "results": [
+      {
+        "adult": false,
+        "backdrop_path": "/jZIYaISP3GBSrVOPfrp98AMa8Ng.jpg",
+        "genre_ids": [
+          16,
+          35,
+          10751,
+          14,
+          10749
+        ],
+        "id": 976573,
+        "original_language": "en",
+        "original_title": "Elemental",
+        "overview": "In a city where fire, water, land and air residents live together, a fiery young woman and a go-with-the-flow guy will discover something elemental: how much they have in common.",
+        "popularity": 2396.197,
+        "poster_path": "/8riWcADI1ekEiBguVB9vkilhiQm.jpg",
+        "release_date": "2023-06-14",
+        "title": "Elemental",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 943
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/2vFuG6bWGyQUzYS9d69E5l85nIz.jpg",
+        "genre_ids": [
+          28,
+          12,
+          878
+        ],
+        "id": 667538,
+        "original_language": "en",
+        "original_title": "Transformers: Rise of the Beasts",
+        "overview": "When a new threat capable of destroying the entire planet emerges, Optimus Prime and the Autobots must team up with a powerful faction known as the Maximals. With the fate of humanity hanging in the balance, humans Noah and Elena will do whatever it takes to help the Transformers as they engage in the ultimate battle to save Earth.",
+        "popularity": 1924.017,
+        "poster_path": "/gPbM0MK8CP8A174rmUwGsADNYKD.jpg",
+        "release_date": "2023-06-06",
+        "title": "Transformers: Rise of the Beasts",
+        "video": false,
+        "vote_average": 7.5,
+        "vote_count": 2770
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/yF1eOkaYvwiORauRCPWznV9xVvi.jpg",
+        "genre_ids": [
+          28,
+          12,
+          878
+        ],
+        "id": 298618,
+        "original_language": "en",
+        "original_title": "The Flash",
+        "overview": "When his attempt to save his family inadvertently alters the future, Barry Allen becomes trapped in a reality in which General Zod has returned and there are no Super Heroes to turn to. In order to save the world that he is in and return to the future that he knows, Barry's only hope is to race for his life. But will making the ultimate sacrifice be enough to reset the universe?",
+        "popularity": 1650.607,
+        "poster_path": "/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg",
+        "release_date": "2023-06-13",
+        "title": "The Flash",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 2319
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/rRcNmiH55Tz0ugUsDUGmj8Bsa4V.jpg",
+        "genre_ids": [
+          35,
+          10749
+        ],
+        "id": 884605,
+        "original_language": "en",
+        "original_title": "No Hard Feelings",
+        "overview": "On the brink of losing her childhood home, Maddie discovers an intriguing job listing: wealthy helicopter parents looking for someone to “date” their introverted 19-year-old son, Percy, before he leaves for college. To her surprise, Maddie soon discovers the awkward Percy is no sure thing.",
+        "popularity": 1860.113,
+        "poster_path": "/4K7gQjD19CDEPd7A9KZwr2D9Nco.jpg",
+        "release_date": "2023-06-15",
+        "title": "No Hard Feelings",
+        "video": false,
+        "vote_average": 7.1,
+        "vote_count": 598
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/nHf61UzkfFno5X1ofIhugCPus2R.jpg",
+        "genre_ids": [
+          35,
+          12,
+          14
+        ],
+        "id": 346698,
+        "original_language": "en",
+        "original_title": "Barbie",
+        "overview": "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
+        "popularity": 1893.5,
+        "poster_path": "/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg",
+        "release_date": "2023-07-19",
+        "title": "Barbie",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 3058
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/zN41DPmPhwmgJjHwezALdrdvD0h.jpg",
+        "genre_ids": [
+          28,
+          878,
+          27
+        ],
+        "id": 615656,
+        "original_language": "en",
+        "original_title": "Meg 2: The Trench",
+        "overview": "An exploratory dive into the deepest depths of the ocean of a daring research team spirals into chaos when a malevolent mining operation threatens their mission and forces them into a high-stakes battle for survival.",
+        "popularity": 1659.416,
+        "poster_path": "/4m1Au3YkjqsxF8iwQy0fPYSxE0h.jpg",
+        "release_date": "2023-08-02",
+        "title": "Meg 2: The Trench",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 456
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/bz66a19bR6BKsbY8gSZCM4etJiK.jpg",
+        "genre_ids": [
+          28,
+          27,
+          53
+        ],
+        "id": 1006462,
+        "original_language": "en",
+        "original_title": "The Flood",
+        "overview": "A horde of giant hungry alligators is unleashed on a group of in-transit prisoners and their guards after a massive hurricane floods Louisiana.",
+        "popularity": 1479.025,
+        "poster_path": "/mvjqqklMpHwOxc40rn7dMhGT0Fc.jpg",
+        "release_date": "2023-07-14",
+        "title": "The Flood",
+        "video": false,
+        "vote_average": 6.9,
+        "vote_count": 119
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/hPcP1kv6vrkRmQO3YgV1H97FE5Q.jpg",
+        "genre_ids": [
+          27,
+          9648,
+          53
+        ],
+        "id": 614479,
+        "original_language": "en",
+        "original_title": "Insidious: The Red Door",
+        "overview": "To put their demons to rest once and for all, Josh Lambert and a college-aged Dalton Lambert must go deeper into The Further than ever before, facing their family's dark past and a host of new and more horrifying terrors that lurk behind the red door.",
+        "popularity": 1382.748,
+        "poster_path": "/d07phJqCx6z5wILDYqkyraorDPi.jpg",
+        "release_date": "2023-07-05",
+        "title": "Insidious: The Red Door",
+        "video": false,
+        "vote_average": 6.9,
+        "vote_count": 855
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/6VWICth8p4zqZiBMKgn6QQ9tnYW.jpg",
+        "genre_ids": [
+          28,
+          12,
+          53,
+          35
+        ],
+        "id": 457332,
+        "original_language": "en",
+        "original_title": "Hidden Strike",
+        "overview": "Two elite soldiers must escort civilians through a gauntlet of gunfire and explosions.",
+        "popularity": 1040.298,
+        "poster_path": "/zsbolOkw8RhTU4DKOrpf4M7KCmi.jpg",
+        "release_date": "2023-07-06",
+        "title": "Hidden Strike",
+        "video": false,
+        "vote_average": 7.1,
+        "vote_count": 552
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/7drO1kYgQ0PnnU87sAnBEphYrSM.jpg",
+        "genre_ids": [
+          16,
+          28,
+          27
+        ],
+        "id": 1083862,
+        "original_language": "ja",
+        "original_title": "バイオハザード：デスアイランド",
+        "overview": "In San Francisco, Jill Valentine is dealing with a zombie outbreak and a new T-Virus, Leon Kennedy is on the trail of a kidnapped DARPA scientist, and Claire Redfield is investigating a monstrous fish that is killing whales in the bay. Joined by Chris Redfield and Rebecca Chambers, they discover the trail of clues from their separate cases all converge on the same location, Alcatraz Island, where a new evil has taken residence and awaits their arrival.",
+        "popularity": 912.474,
+        "poster_path": "/xzAQ28moSPEZxOHJ7WL1mX6hb5H.jpg",
+        "release_date": "2023-06-22",
+        "title": "Resident Evil: Death Island",
+        "video": false,
+        "vote_average": 7.7,
+        "vote_count": 539
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/aLpQ3G2LRgXYNrQgUlo6AQRo9R6.jpg",
+        "genre_ids": [
+          28,
+          53
+        ],
+        "id": 1143190,
+        "original_language": "en",
+        "original_title": "Fear the Night",
+        "overview": "During a bachelorette party in a secluded California farmhouse, masked intruders launch a brutal attack, forcing eight women to fight for survival. Led by Tess, a troubled military veteran, they unite to defend themselves throughout a harrowing night.",
+        "popularity": 643.319,
+        "poster_path": "/4XLZS2xvdv5rxizzTUVREtRyw95.jpg",
+        "release_date": "2023-07-21",
+        "title": "Fear the Night",
+        "video": false,
+        "vote_average": 6.7,
+        "vote_count": 92
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/iJ0UZaC7XW7BUpRQ7OLPZSms8Ou.jpg",
+        "genre_ids": [
+          28,
+          12,
+          18,
+          14,
+          878
+        ],
+        "id": 813477,
+        "original_language": "ja",
+        "original_title": "シン・仮面ライダー",
+        "overview": "A man forced to bear power and stripped of humanity. A woman skeptical of happiness. Takeshi Hongo, an Augmentation made by SHOCKER, and Ruriko Midorikawa, a rebel of the organization, escape while fighting off assassins. What’s justice? What’s evil? Will this violence end? Despite his power, Hongo tries to remain human. Along with freedom, Ruriko has regained a heart. What paths will they choose?",
+        "popularity": 742.951,
+        "poster_path": "/9dTO2RygcDT0cQkawABw4QkDegN.jpg",
+        "release_date": "2023-03-17",
+        "title": "Shin Kamen Rider",
+        "video": false,
+        "vote_average": 7.2,
+        "vote_count": 154
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/iEFuHjqrE059SmflBva1JzDJutE.jpg",
+        "genre_ids": [
+          16,
+          10751,
+          28,
+          14,
+          10749
+        ],
+        "id": 496450,
+        "original_language": "fr",
+        "original_title": "Miraculous - le film",
+        "overview": "A life of an ordinary Parisian teenager Marinette goes superhuman when she becomes Ladybug. Bestowed with magical powers of creation, Ladybug must unite with her opposite, Cat Noir, to save Paris as a new villain unleashes chaos unto the city.",
+        "popularity": 756.574,
+        "poster_path": "/dQNJ8SdCMn3zWwHzzQD2xrphR1X.jpg",
+        "release_date": "2023-07-05",
+        "title": "Miraculous: Ladybug & Cat Noir, The Movie",
+        "video": false,
+        "vote_average": 7.9,
+        "vote_count": 464
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/f7UI3dYpr7ZUHGo0iIr1Qvy1VPe.jpg",
+        "genre_ids": [
+          16,
+          10751,
+          14,
+          35
+        ],
+        "id": 1040148,
+        "original_language": "en",
+        "original_title": "Ruby Gillman, Teenage Kraken",
+        "overview": "Ruby Gillman, a sweet and awkward high school student, discovers she's a direct descendant of the warrior kraken queens. The kraken are sworn to protect the oceans of the world against the vain, power-hungry mermaids. Destined to inherit the throne from her commanding grandmother, Ruby must use her newfound powers to protect those she loves most.",
+        "popularity": 708.075,
+        "poster_path": "/kgrLpJcLBbyhWIkK7fx1fM4iSvf.jpg",
+        "release_date": "2023-06-28",
+        "title": "Ruby Gillman, Teenage Kraken",
+        "video": false,
+        "vote_average": 7.6,
+        "vote_count": 530
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/lDCIQ1Qe7cRnhZ4ybQVVEbadMZ.jpg",
+        "genre_ids": [
+          27,
+          53
+        ],
+        "id": 1008042,
+        "original_language": "en",
+        "original_title": "Talk to Me",
+        "overview": "When a group of friends discover how to conjure spirits using an embalmed hand, they become hooked on the new thrill, until one of them goes too far and unleashes terrifying supernatural forces.",
+        "popularity": 805.927,
+        "poster_path": "/kdPMUMJzyYAc4roD52qavX0nLIC.jpg",
+        "release_date": "2023-07-26",
+        "title": "Talk to Me",
+        "video": false,
+        "vote_average": 7.1,
+        "vote_count": 165
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg",
+        "genre_ids": [
+          18,
+          36
+        ],
+        "id": 872585,
+        "original_language": "en",
+        "original_title": "Oppenheimer",
+        "overview": "The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.",
+        "popularity": 736.078,
+        "poster_path": "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+        "release_date": "2023-07-19",
+        "title": "Oppenheimer",
+        "video": false,
+        "vote_average": 8.3,
+        "vote_count": 1962
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/14GEZCzCGhV7FMFaWi4Ec22Kcai.jpg",
+        "genre_ids": [
+          16,
+          12,
+          10751,
+          14
+        ],
+        "id": 459003,
+        "original_language": "uk",
+        "original_title": "Мавка: Лісова пісня",
+        "overview": "Mavka — a Soul of the Forest and its Warden — faces an impossible choice between love and her duty as guardian to the Heart of the Forest, when she falls in love with a human — the talented young musician Lukas.",
+        "popularity": 606.751,
+        "poster_path": "/eeJjd9JU2Mdj9d7nWRFLWlrcExi.jpg",
+        "release_date": "2023-03-02",
+        "title": "Mavka: The Forest Song",
+        "video": false,
+        "vote_average": 7.4,
+        "vote_count": 309
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/r54HQwvisSXMfip7oJNhPSWyCK5.jpg",
+        "genre_ids": [
+          28,
+          12,
+          10752
+        ],
+        "id": 1061181,
+        "original_language": "ja",
+        "original_title": "キングダム 運命の炎",
+        "overview": "It follows Li Xin and Wang Qi as they stand on the battlefield for the first time to fight off an invasion by Zhao, and it also follows Ying Zheng's unknown past.",
+        "popularity": 618.587,
+        "poster_path": "/lm5LF2eyCcBdCEfvpeyvpujOyPb.jpg",
+        "release_date": "2023-07-28",
+        "title": "Kingdom 3: The Flame of Fate",
+        "video": false,
+        "vote_average": 8.2,
+        "vote_count": 38
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/oqP1qEZccq5AD9TVTIaO6IGUj7o.jpg",
+        "genre_ids": [
+          14,
+          28,
+          12
+        ],
+        "id": 455476,
+        "original_language": "en",
+        "original_title": "Knights of the Zodiac",
+        "overview": "When a headstrong street orphan, Seiya, in search of his abducted sister unwittingly taps into hidden powers, he discovers he might be the only person alive who can protect a reincarnated goddess, sent to watch over humanity. Can he let his past go and embrace his destiny to become a Knight of the Zodiac?",
+        "popularity": 543.702,
+        "poster_path": "/qW4crfED8mpNDadSmMdi7ZDzhXF.jpg",
+        "release_date": "2023-04-27",
+        "title": "Knights of the Zodiac",
+        "video": false,
+        "vote_average": 6.7,
+        "vote_count": 739
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/waBWlJlMpyFb7STkFHfFvJKgwww.jpg",
+        "genre_ids": [
+          28,
+          18
+        ],
+        "id": 678512,
+        "original_language": "en",
+        "original_title": "Sound of Freedom",
+        "overview": "The story of Tim Ballard, a former US government agent, who quits his job in order to devote his life to rescuing children from global sex traffickers.",
+        "popularity": 445.018,
+        "poster_path": "/kSf9svfL2WrKeuK8W08xeR5lTn8.jpg",
+        "release_date": "2023-07-03",
+        "title": "Sound of Freedom",
+        "video": false,
+        "vote_average": 8.2,
+        "vote_count": 362
+      }
+    ],
+    "total_pages": 75,
+    "total_results": 1494
+  }
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { dummy } from '../movieDummy'
+import Movie from '../components/Movie'
 
 export default function Movies() {
-  return (
-    <div
-    className='page-container'
-    style={{
-        fontSize: "32px"
-    }}>Movies</div>
-  )
+    return (
+        <div>
+            <div className="app-container">
+                {
+                    dummy.results.map((item) => {
+                        return (
+                            <Movie
+                                title={item.title}
+                                poster_path={item.poster_path}
+                                vote_average={item.vote_average}
+                            />
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
 }
