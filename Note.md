262feb38151bdff00a359502544ba34a102a13a7
mjlee0509 <mjlee0509@gmail.com>
2023-09-13 15:55:08 +0900

    영화 앱 만들기 II #2. 라우팅(Routing) 및 링크 만들기

---
+++ Note.md
@@ -0,0 +1,2 @@
+1. 라우팅 작업을 위해 react-router-dom 설치
+  - npm i react-router-dom
--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 
+import Celebs from "./pages/Celebs";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
+import Tv from "./pages/Tv";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-    </div>
+      <BrowserRouter>
+      <Header /> {/* Routing의 영향을 받지 않고 고정시키고 싶은 컴포넌트는 Routes 태그 밖으로 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movies />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/person" element={<Celebs />} />
+        <Route path="/*" element={<NotFound />} /> {/* 위에 해당하지 않는 주소로 이동했을 때에는 NotFound 페이지 띄우기*/}
+      </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
 
